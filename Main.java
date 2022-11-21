4f78cb8606cec6fe056144018d4afa52a8aac4fa
Muhammad Ghora Samudra <113771792+muhammadghora@users.noreply.github.com>
2022-11-21 20:21:17 +0700

    Nomor 2

--- Main.java
+++ Main.java
@@ -1,62 +1,83 @@
-package quiz_utama_pbo;
+package quiz_utama_pbo1;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner terminalInput = new Scanner(System.in);
-        String inputUser, pilihanUser;
-        Login user = new Login();
-        // ==============================
-        System.out.println("\t--- Distributor PT. Garuda Abadi Group ---");
-        System.out.println("\t============================================");
+        String nama, nik, alamat, tlp, pekerjaan, kantor, status, menikah, jml_anak, input;
+        int sampah, keamanan, sumbangan;
+        iuran iuran = new iuran();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("======================================");
+        System.out.println(" Pendataan dan Pembayaran Iuran Warga ");
+        System.out.println("======================================");
+        System.out.println("1. Input Data Warga");
+        System.out.println("2. Input Iuran Warga");
         System.out.println("");
-        System.out.println("Silahkan Masukkan ID Anda");
-        System.out.println("===========================");
-        System.out.print("ID : ");
-        // Ambil input id user
-        inputUser = terminalInput.next();
-        // Cek id user
-        if (user.cekUser(inputUser)) {
-            System.out.println("");
-            System.out.println("Pilih Cek Data Barang");
-            System.out.println("1. Barang Masuk");
-            System.out.println("2. Barang Keluar");
-            System.out.println("");
-            System.out.print("Masukkan Pilihan Data :\t");
-            // Ambil pilihan user
-            pilihanUser = terminalInput.next();
-            System.out.println("=============================");
-            System.out.println("");
+        System.out.print("PILIHAN MENU : ");
+        input = sc.next();
+        System.out.println("----------------------o0o----------------------");
+        if (input.equals("1")) {
+            System.out.println("*** Data Warga ***");
+            System.out.println("Masukan Nama : ");
+            nama = sc.next();
+            System.out.println("Masukan NIK : ");
+            nik = sc.next();
+            System.out.println("Masukan Alamat: ");
+            alamat = sc.next();
+            System.out.println("Masukan No. Tlp : ");
+            System.out.print("+62 ");
+            tlp = sc.next();
+            // =====
+            iuran.data(nama, nik, alamat, tlp);
+            // =====
+            System.out.println("-------------------o0o---------------------");
+            System.out.println("*** Data Pekerjaan ***");
+            System.out.println("Masukan Pekerjaan :");
+            pekerjaan = sc.next();
+            System.out.println("Masukan Alamat Kantor :");
+            kantor = sc.next();
+            // =====
+            iuran.data(pekerjaan, kantor);
+            // =====
+            System.out.println("------------------o0o----------------------");
+            System.out.println("*** Data Status ***");
+            System.out.println("Masukan Status :");
+            status = sc.next();
+            System.out.println("Masukan Nama Suami/Istri :");
+            menikah = sc.next();
+            System.out.println("Masukan Jumlah Anak :");
+            jml_anak = sc.next();
+            // ======
+            iuran.data(status, menikah, jml_anak);
+            // ==========================
+            System.out.println("Lanjut Bayar Iuran ? Y/T");
+            input = sc.next();
+            if (input.equals("Y")) {
+                System.out.println("----------------------o0o----------------------");
+                System.out.println("*** Data Iuran Pokok ***");
+                System.out.println("NIK = > " + iuran.nik);
+                System.out.println("Masukkan Iuran Sampah :");
+                sampah = sc.nextInt();
+                System.out.println("Masukkan Iuran Keamanan :");
+                keamanan = sc.nextInt();
+                System.out.println("");
+                System.out.println("*** Data Iuran Sekunder ***:");
+                System.out.println("Masukkan Sumbangan :");
+                sumbangan = sc.nextInt();
+                System.out.println("Lanjutkan Bayar Iuran : T");
+                input = sc.next();
+                if (input.equals("T")) {
+                    System.out.println("NIK =>" + iuran.nik);
+                    System.out.println("Total Pembayaran Iuran Warga => " + iuran.data(sampah, keamanan, sumbangan));
+                    System.out.println("Terimakasih " + iuran.nama + " Iuran Anda Sudah di Bayarkan");
 
-            if (pilihanUser.equals("1")) {
-                BarangMasuk masuk = new BarangMasuk();
-                // ================================
-                masuk.data();
-                masuk.list();
-                masuk.hitung();
+                } else {
+                }
 
-            } else if (pilihanUser.equals("2")) {
-                BarangKeluar keluar = new BarangKeluar();
-                // ================================
-                keluar.data();
-                keluar.list();
-                keluar.hitung();
-
-            } else {
-                System.exit(0);
             }
-
-        } else
-
-        {
-            System.out.println("ID Anda Salah => " + inputUser);
-            System.out.println("");
-            System.out.println("");
-            System.out.println("Administrasi Persediaan Barang Distibutor PT. Garuda Abadi Group");
-            System.out.println("Check, Data, dan Antar, Ketelitian Anda dalam Administrasi Sangat Di Utamakan (*_*)");
         }
-
     }
 
 }
